9d4237d2693aa0b8d102f7c9a2ab684093a9ea37
udanuj07 <anuj2977013@gmail.com>
2025-11-27 13:03:42 +0530

    Add JavaDoc comments to User.java model class

--- User.java
+++ User.java
@@ -1,9 +1,14 @@
 package project;
 
+/**
+ * User model class representing a user in the ResolveIt system.
+ * Contains user attributes: userId, username, password, and role.
+ */
 public class User {
     public int userId;
     public String username;
     public String password;
     public String role;
 }
+
 
